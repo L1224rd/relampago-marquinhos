6846b2f76a9c9744759ee5f2e20b2fd99645af37
Yorrmungand <edgarmarques@hotmail.com.br>
2019-01-25 17:55:00 -0200

    edited dist function

--- app.js
+++ app.js
@@ -7,9 +7,24 @@
  *    worst: {car: string, dist: number},
  *    avgDist: number
  * };
- * 
+ *
  * procriate(Array[string]) => Array[string];
  * 
  * dist(string) => number;
  * 
  */
+
+
+ const dist = (car) => {
+    let dis = 0;
+
+    for(let i=0;i<car.length;i++) {
+        if(car[i] === 1) {
+            dis += 1;
+        }
+        if(car[i] === 3) {
+            dis -= 1;
+        }
+    }
+    return dis;
+}
