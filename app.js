45803d0facb14bdc84b02555ea5dcd0c9c61ce12
Eder Marques <eder.marques@citi.org.br>
2019-01-25 16:39:26 -0300

    added car Architecture

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Car() => string;
+ * 
+ * stats(string || Array[string]) => {
+ * best: {
+ *    best: {car: string, dist: number},
+ *    worst: {car: string, dist: number},
+ *    avgDist: number
+ * };
+ * 
+ * procriate(Array[string]) => Array[string];
+ * 
+ * dist(string) => number;
+ * 
+ */
